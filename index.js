dbf773ed7c7544502012a5d7f176739ac3da433d
Anthony Aoun <mindoftoto@gmail.com>
2019-04-21 18:19:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-import { getHTML, getTwitterFollowers } from './lib/scraper';
+import {
+  getHTML,
+  getTwitterFollowers,
+  getInstagramFollowers
+} from './lib/scraper';
 
 async function go() {
-  const html = await getHTML('https://twitter.com/anthonyaoun23');
-  const twitterCount = await getTwitterFollowers(html);
+  const igPromise = getHTML('https://instagram.com/mindoftoto');
+  const twPromise = getHTML('https://twitter.com/anthonyaoun23');
+  const [igHTML, twHTML] = await Promise.all([igPromise, twPromise]);
+  const igCount = await getInstagramFollowers(igHTML);
+  const twCount = await getTwitterFollowers(twHTML);
 
-  console.log(`You have ${twitterCount} followers on twitter.`);
+  console.log(
+    `You have ${twCount} followers on twitter and ${igCount} followers on instagram.`
+  );
 }
 
 go();
