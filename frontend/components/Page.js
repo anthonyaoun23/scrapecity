e56f3a3cd5c1e75e32ae267304185d2d88864582
Anthony Aoun <mindoftoto@gmail.com>
2019-04-25 00:34:46 -0400

    add fetching with a custom hook

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react';
 import { ScrapeProvider } from './ScrapeContext';
 
+// Custom hook
+function useScrapes() {
+  const [scrapes, setScrapes] = useState({});
+  useEffect(
+    (async () => {
+      console.log('Mounting or updating.');
+      const res = await fetch('http://localhost:3001/data');
+      const data = await res.json();
+      console.log(data);
+      setScrapes(data);
+    })(),
+    []
+  );
+
+  return scrapes;
+}
+
 export default function Page({ children }) {
+  const scrapes = useScrapes();
   return (
-    <ScrapeProvider value={{ hey: 'ho', lets: 'go' }}>
+    <ScrapeProvider value={{ scrapes }}>
       <div className='page'>{children}</div>
     </ScrapeProvider>
   );
