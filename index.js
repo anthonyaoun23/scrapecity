69981595fdb56fd5f3f61b5412aa458dce30ae13
Anthony Aoun <mindoftoto@gmail.com>
2019-04-24 22:34:01 -0400

    abstract the follower fetching functions

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-import {
-  getHTML,
-  getTwitterFollowers,
-  getInstagramFollowers
-} from './lib/scraper';
+import { getInstagramCount, getTwitterCount } from './lib/scraper';
 
 async function go() {
-  const igPromise = getHTML('https://instagram.com/mindoftoto');
-  const twPromise = getHTML('https://twitter.com/anthonyaoun23');
-  const [igHTML, twHTML] = await Promise.all([igPromise, twPromise]);
-  const igCount = await getInstagramFollowers(igHTML);
-  const twCount = await getTwitterFollowers(twHTML);
+  const [igCount, twCount] = await Promise.all([
+    getInstagramCount(),
+    getTwitterCount()
+  ]);
 
   console.log(
     `You have ${twCount} followers on twitter and ${igCount} followers on instagram.`
